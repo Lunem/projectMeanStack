1fbd6cc1e2920a5894510e0b7cbdbcd00d67b9d6
Lunem <lydia.detienne@gmail.com>
2016-06-02 22:15:12 +0200

    next

--- server.js
+++ server.js
@@ -15,13 +15,45 @@ adminRouter.get('/', function(req, res) {
    res.send('I show all the posts!');
  });
 
+ adminRouter.get('/hello/:name', function(req, res) {
+ res.send('hello ' + req.params.name + '!');
+ });
+
+ app.route('/login')
+        //  (GET http://localhost:1337/login)
+.get(function(req, res) {
+res.send('this is the login form');
+})
+        //  (POST http://localhost:1337/login)
+.post(function(req, res) { console.log('processing');
+                res.send('processing the login form!');
+});
+
+// route middleware to validate :name
+ adminRouter.param('name', function(req, res, next, name) {
+// do validation on name here
+// blah blah validation
+ // log something so we know its working
+ console.log('doing name validations on ' + name);
+
+ // once validation is done save the new item in the req
+ req.name = name;
+ // go to the next thing
+ next();
+ });
+
+// route with parameters (http://localhost:1337/admin/hello/:name)
+ adminRouter.get('/hello/:name', function(req, res) {
+res.send('hello ' + req.name + '!');
+ });
+
 app.use('/admin', adminRouter); //recupere les routes
 
 app.get('/', function(req, res) {
 res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(1337); // sorry
+app.listen(1337);
 console.log('1337 is the magic port!');
 
 // La methode 'traditionnelle';

eaa9b7736c1d942162f302bab2175ec3bba67a96
Lunem <lydia.detienne@gmail.com>
2016-06-02 16:06:32 +0200

    routes

--- server.js
+++ server.js
@@ -1,1 +1,45 @@
-console.log('recoucou');
+var express = require('express');
+var app  = express();
+var path = require('path');
+var adminRouter = express.Router();
+
+adminRouter.get('/', function(req, res) {
+   res.send('I am the dashboard!');
+ });
+
+ adminRouter.get('/users', function(req, res) {
+   res.send('I show all the users!');
+ });
+
+ adminRouter.get('/posts', function(req, res) {
+   res.send('I show all the posts!');
+ });
+
+app.use('/admin', adminRouter);
+
+app.get('/', function(req, res) {
+res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(1337); // sorry
+console.log('1337 is the magic port!');
+
+// La methode 'traditionnelle';
+
+// var http = require('http'),
+//  fs = require('fs');
+//
+// http.createServer(function(req, res) {
+//
+//   res.writeHead(200, {
+//     'Content-Type': 'text/html',
+//     'Access-Control-Allow-Origin' : '*'
+// });
+//
+// var readStream = fs.createReadStream(__dirname + '/index.html');
+//
+//
+//  readStream.pipe(res);
+// }).listen(1337);
+//
+// console.log('Visit me at http://localhost:1337');
